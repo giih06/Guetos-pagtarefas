8e87f787b07f195ec2dff476658d65d34be2723a
Emanuel <101946589+mano3queijos@users.noreply.github.com>
2023-11-02 15:33:00 -0300

    index

--- js/main.js
+++ js/main.js
@@ -4,3 +4,28 @@ searchButton.addEventListener('click', () => {
   const inputValue = searchInput.value;
   alert(inputValue);
 });
+
+
+function dados (){
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+
+}
+
+}
+const dados = [
+  { nome: "João", idade: 25 },
+  { nome: "Maria", idade: 30 },
+  { nome: "Pedro", idade: 22 },
+];
+
+            async function getRandomCatImage() {
+                        const response = await fetch(
+                          "https://api.thecatapi.com/v1/images/search"
+                        );
+                        const data = await response.json();
+                        return data[0].url;
+                      }
+
+                      getRandomCatImage();
