2c294be284508e259d26f3dd636fd28c92f2b3ff
Emanuel <101946589+mano3queijos@users.noreply.github.com>
2023-11-04 21:34:55 -0300

    cat api ok

--- js/main.js
+++ js/main.js
@@ -1,31 +1,57 @@
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
-searchButton.addEventListener('click', () => {
-  const inputValue = searchInput.value;
-  alert(inputValue);
-});
+const projetos = [];
+const statusOptions = ["Canceled", "In Progress", "Delivered", "Attention!"];
 
+// Função para gerar projetos aleatórios
+async function generateProjects(valor) {
+  projetos.length = 0;
 
-function dados (){
+  for (let i = 1; i <= valor; i++) {
+    const projeto = {
+      ProjectID: `${i}`,
+      Projects: `Projeto ${i}`,
+      EST: Math.floor(Math.random() * 101), // Progresso aleatório
+      Status: statusOptions[Math.floor(Math.random() * statusOptions.length)],
+      CatImages: [],
+    };
 
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
-
+    for (let j = -2; j < Math.floor(Math.random() * 3); j++) {
+      const catImageResponse = await fetch(
+        "https://api.thecatapi.com/v1/images/search"
+      );
+      const catImageData = await catImageResponse.json();
+      projeto.CatImages.push(catImageData[0].url); // URL da imagem de gatinho
+    }
+    projetos.push(projeto);
+  }
+  displayProjectsInTable(projetos);
 }
 
+// Função para exibir os projetos na tabela
+async function displayProjectsInTable(projetos) {
+  const tbody = document.querySelector("tbody");
+  tbody.innerHTML = ""; // Limpa o conteúdo atual da tabela
+
+  projetos.forEach((projeto, index) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+                          <th scope="row">${projeto.ProjectID}</th>
+                          <td>
+                            ${projeto.Projects}<br />
+                      
+                          </td>
+                          <td>Completion with: ${
+                            projeto.EST
+                          }% <div class="progress"><div class="progress-bar" role="progressbar" style="width: ${
+      projeto.EST
+    }%" aria-valuenow="${
+      projeto.EST
+    }" aria-valuemin="0" aria-valuemax="100"></div></div></td>
+                          <td>${projeto.Status}</td>
+                          <td>
+                                  ${projeto.CatImages.map(
+                                    (catImage) => `<img src="${catImage}" />`
+                                  ).join("")}</td>
+                          `;
+    tbody.appendChild(row);
+  });
 }
-const dados = [
-  { nome: "João", idade: 25 },
-  { nome: "Maria", idade: 30 },
-  { nome: "Pedro", idade: 22 },
-];
-
-            async function getRandomCatImage() {
-                        const response = await fetch(
-                          "https://api.thecatapi.com/v1/images/search"
-                        );
-                        const data = await response.json();
-                        return data[0].url;
-                      }
-
-                      getRandomCatImage();
